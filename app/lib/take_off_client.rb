6d56a0ed9786fb37b6f35885ab14787d9ddfba47
mzuccotti <mzuccotti@frba.utn.edu.ar>
2023-07-25 21:33:46 -0300

    Use make_request on all client requests and add request for Get Flight Coordinator Endpoint

--- app/lib/take_off_client.rb
+++ app/lib/take_off_client.rb
@@ -6,55 +6,46 @@ class TakeOffClient
   end
 
   def health_check
-    response = connection.get("/health-check")
-    JSON.parse(response.body)
+    make_request(:get, "/health-check")
   end
 
   def flights_create(flight)
-    response = connection.post("/flights", flight.to_json)
-    JSON.parse(response.body)
+    make_request(:post, "/flights", flight.to_json)
   end
 
   def flights_list()
-    response = connection.get("/flights")
-    JSON.parse(response.body)
+    make_request(:get, "/flights")
   end
 
   def flights_subscribe(flight_id, user)
-    response = connection.post(
-      "/flights/#{flight_id}/subscriptions",
-      {
-        user: user,
-        webhook_uri: "http://localhost:8080/webhooks"
-      }.to_json
-    )
-    JSON.parse(response.body)
+    make_request(:post, "/flights/#{flight_id}/subscriptions", {
+      user: user,
+      webhook_uri: "http://localhost:8080/webhooks"
+    }.to_json)
   end
 
   def flights_subscriptions_list(flight_id)
-    response = connection.get("/flights/#{flight_id}/subscriptions")
-    JSON.parse(response.body)
+    make_request(:get, "/flights/#{flight_id}/subscriptions")
+  end
+
+  def flights_get_supervisor_node(flight_id)
+    make_request(:get, "/flights/#{flight_id}/supervisor")
   end
 
   def bookings_create(booking)
     make_request(:post, "/reservations", booking.to_json)
-    # response = connection.post("/reservations", booking.to_json)
-    # JSON.parse(response.body)
   end
 
   def bookings_list()
-    response = connection.get("/reservations")
-    JSON.parse(response.body)
+    make_request(:get, "/reservations")
   end
 
   def alerts_create(alert)
-    response = connection.post("/alerts", alert.to_json)
-    JSON.parse(response.body)
+    make_request(:post, "/alerts", alert.to_json)
   end
 
   def alerts_list()
-    response = connection.get("/alerts")
-    JSON.parse(response.body)
+    make_request(:get, "/alerts")
   end
 
   private
@@ -63,7 +54,6 @@ class TakeOffClient
     response = connection.send(method, path, body)
     JSON.parse(response.body)
   rescue => e
-    binding.pry
     puts "Error. Response: #{response.inspect}"
     raise e
   end
