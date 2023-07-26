5e665ef43eab705d32cdcc903fc215bc2c2efbc7
mzuccotti <mzuccotti@frba.utn.edu.ar>
2023-07-26 00:23:43 -0300

    Improve Take Off Node interface and clean up specs

--- app/lib/take_off_node.rb
+++ app/lib/take_off_node.rb
@@ -1,24 +1,31 @@
 class TakeOffNode
   attr_accessor :nodes
+  attr_reader :base_port
+  attr_reader :created_nodes
 
-  def initialize
+  def initialize(base_port = 27700)
+    @base_port = base_port
+    @created_nodes = 0
     @nodes = {}
   end
 
-  def start_node(port)
-    add_node(port)
-    validate_node_alive(port)
+  def start_node
+    port = base_port + created_nodes
+    name = add_node(port)
+    validate_node_alive(name)
+    name
   end
 
-  def start_nodes(ports)
-    ports.each { |port| add_node(port) }
-    ports.each do |port|
-      validate_node_alive(port)
+  def start_nodes(count)
+    ports = count.times.map { |i| base_port + created_nodes + i }
+    node_names = ports.map { |port| add_node(port) }
+    node_names.each do |name|
+      validate_node_alive(name)
     end
   end
 
   def stop_node(name)
-    `kill -9 $(lsof -s TCP:LISTEN -ti:#{nodes[name]})`
+    `kill -9 $(lsof -s TCP:LISTEN -ti:#{nodes[name][:port]})`
     nodes.delete(name)
   end
 
@@ -26,26 +33,42 @@ class TakeOffNode
     nodes.each { |name, _| stop_node(name) }
   end
 
+  def client(name)
+    nodes[name][:client]
+  end
+
+  def some_client
+    nodes.values.sample[:client]
+  end
+
+  def node_names
+    nodes.keys
+  end
+
+  def clients
+    nodes.values.map { |node| node[:client] }
+  end
+
   private
 
   def add_node(port)
     name = "node-#{port}"
     `cd ../iasc-take-off && PORT=#{port} elixir --erl "-detached" --name #{name}@127.0.0.1 -S mix phx.server`
-    nodes[name] = port
+    nodes[name] = {port: port, client: TakeOffClient.new("http://localhost:#{port}")}
+    @created_nodes += 1
+    name
   end
 
-  def validate_node_alive(port)
-    client = TakeOffClient.new("http://localhost:#{port}")
-
+  def validate_node_alive(name)
     10.times do
-      puts "Validating health of node #{port}"
-      client.health_check
-      puts "Node #{port} ready"
+      puts "Validating health of node #{name}"
+      client(name).health_check
+      puts "Node #{name} ready"
       return
     rescue StandardError => e
       sleep(2)
     end
 
-    raise StandardError, "Node #{port} could not be started"
+    raise StandardError, "Node #{name} could not be started"
   end
 end
--- spec/lib/take_off_client_spec.rb
+++ spec/lib/take_off_client_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe TakeOffClient do
-  def client(port)
-    described_class.new("http://localhost:#{port}")
-  end
-
   context 'when all goes good' do
     before(:all) do
+      @node_manager = TakeOffNode.new(27220)
       sleep(5)
-      @node_manager = TakeOffNode.new
-      @node_manager.start_nodes([27701, 27702])
     end
 
     after(:all) do
@@ -17,81 +12,79 @@ RSpec.describe TakeOffClient do
     end
 
     it 'creates a flight' do
-      clients = @node_manager.nodes.values.map { |port| [port, client(port)] }.to_h
+      @node_manager.start_nodes(2)
 
       # Create an alert
       alert = FactoryBot.build(:alert, :with_date, date: "2025-06-24")
-      response = clients.values.sample.alerts_create(alert)
+      response = @node_manager.some_client.alerts_create(alert)
       expect(response).to eq("status" => "ok")
 
-      # The alert exists in all nodes
-      clients.values.each do |client|
+      # The alert should exist in all nodes
+      @node_manager.clients.each do |client|
         response = client.alerts_list()
         expect(response['value'].first['user']).to eq(alert[:user])
       end
 
       # Create a flight
       flight = FactoryBot.build(:flight, datetime: "2025-06-24T00:00:00Z", seats: { window: 25, aisle: 25, between_seats: 25 })
-      response = clients.values.sample.flights_create(flight)
+      response = @node_manager.some_client.flights_create(flight)
       expect(response).to include("status" => "ok")
 
-      # The flight exists in all nodes
-      clients.values.each do |client|
+      # The flight should exist in all nodes
+      @node_manager.clients.each do |client|
         response = client.flights_list()
         expect(response['value'].values.first['origin']).to eq(flight[:origin])
       end
 
-      # Start a new node and check to see if it has the flight
-      @node_manager.start_node(27704)
-      clients[27704] = client(27704)
-      response = clients[27704].flights_list()
+      # Start a new node and check if it has the flight
+      new_node_name = @node_manager.start_node
+      response = @node_manager.client(new_node_name).flights_list()
       expect(response['value'].values.first['origin']).to eq(flight[:origin])
 
       # Subscribe to the flight
       flight_id = response['value'].values.first['id']
-      response = clients.values.sample.flights_subscribe(flight_id, "user")
+      response = @node_manager.some_client.flights_subscribe(flight_id, "user")
       expect(response).to eq("status" => "ok")
 
       # The subscription exists in all nodes
-      clients.values.each do |client|
+      @node_manager.clients.each do |client|
         response = client.flights_subscriptions_list(flight_id)
         expect(response['value'].first['user']).to eq("user")
       end
 
       # Delete a node to check everything keeps working
-      @node_manager.stop_node("node-27701")
-      clients.delete(27701)
+      @node_manager.stop_node(@node_manager.node_names.first)
 
       # Create a booking
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 1, aisle: 0, between_seats: 0 })
-      response = clients.values.sample.bookings_create(booking)
+      response = @node_manager.some_client.bookings_create(booking)
       expect(response).to include("status" => "booking_accepted")
 
       # The booking exists in all nodes
-      clients.values.each do |client|
+      @node_manager.clients.each do |client|
         response = client.bookings_list()
         expect(response['value'].first['user']).to eq(booking[:user])
       end
 
-      # The subscription was deleted
-      response = clients.values.sample.flights_subscriptions_list(flight_id)
+      # The subscription is deleted
+      response = @node_manager.some_client.flights_subscriptions_list(flight_id)
       expect(response['value']).to be_empty
 
       # Other user creates a subscription
-      response = clients.values.sample.flights_subscribe(flight_id, "other_user")
+      response = @node_manager.some_client.flights_subscribe(flight_id, "other_user")
       expect(response).to eq("status" => "ok")
 
       # Buy all the available seats
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 24, aisle: 25, between_seats: 25 })
-      response = clients.values.sample.bookings_create(booking)
+      response = @node_manager.some_client.bookings_create(booking)
       expect(response).to include("status" => "booking_accepted")
 
-      # The flight was closed
-      response = clients.values.sample.flights_list()
+      # The flight is closed
+      response = @node_manager.some_client.flights_list()
       expect(response['value'].values.first['status']).to eq("closed")
 
-      # The subscription of 'other_user' was deleted
-      response = clients.values.sample.flights_subscriptions_list(flight_id)
+      # The subscription of 'other_user' is deleted
+      response = @node_manager.some_client.flights_subscriptions_list(flight_id)
       expect(response['value']).to be_empty
     end
   end
@@ -99,8 +92,7 @@ RSpec.describe TakeOffClient do
   context 'when the coordinator node goes down' do
     before(:all) do
       sleep(5)
-      @node_manager = TakeOffNode.new
-      @node_manager.start_nodes(2.times.map { |i| 27500 + i } )
+      @node_manager = TakeOffNode.new(27240)
     end
 
     after(:all) do
@@ -108,11 +100,11 @@ RSpec.describe TakeOffClient do
     end
 
     it 'other coordintor is created to confirm bookings' do
-      clients = @node_manager.nodes.map { |name, port| [name, client(port)] }.to_h
+      @node_manager.start_nodes(2)
 
-      # Create a flight
+      # Create a flight and validate it is created
       flight = FactoryBot.build(:flight, datetime: "2025-06-24T00:00:00Z", seats: { window: 2, aisle: 0, between_seats: 0 })
-      response = clients.values.first.flights_create(flight)
+      response = @node_manager.some_client.flights_create(flight)
       flight_id = response['value']['id']
       expect(response).to include("status" => "ok")
 
@@ -120,31 +112,31 @@ RSpec.describe TakeOffClient do
 
       # Create a booking
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 1, aisle: 0, between_seats: 0 })
-      response = clients.values.sample.bookings_create(booking)
+      response = @node_manager.some_client.bookings_create(booking)
       expect(response).to include("status" => "booking_accepted")
 
-      # Get the coordinator node
-      response = clients.values.first.flights_get_coordinator_node(flight_id)
+      # Get the coordinator node and validate the name is correct
+      response = @node_manager.clients.first.flights_get_coordinator_node(flight_id)
       coordinator_node_name = response['value'].split('@').first
+      expect(coordinator_node_name).to include("node")
 
       # Stop the coordinator node
       @node_manager.stop_node(coordinator_node_name)
 
       sleep(1)
 
-      # Get the new coordinator node
-      alive_client = clients.find { |name, _| name != coordinator_node_name }.last
-      response = alive_client.flights_get_coordinator_node(flight_id)
+      # Get the new coordinator node. It should be different from the previous one
+      response = @node_manager.some_client.flights_get_coordinator_node(flight_id)
       new_coordinator_node_name = response['value'].split('@').first
       expect(new_coordinator_node_name).to_not eq(coordinator_node_name)
 
-      # Create two new bookings
+      # Create two new bookings, one should be accepted and the other one denied
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 1, aisle: 0, between_seats: 0 })
-      response = alive_client.bookings_create(booking)
+      response = @node_manager.some_client.bookings_create(booking)
       expect(response).to include("status" => "booking_accepted")
 
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 1, aisle: 0, between_seats: 0 })
-      response = alive_client.bookings_create(booking)
+      response = @node_manager.some_client.bookings_create(booking)
       expect(response).to include("status" => "flight_closed")
     end
   end
@@ -152,8 +144,7 @@ RSpec.describe TakeOffClient do
   context 'when multiple people try to book the same seats' do
     before(:all) do
       sleep(5)
-      @node_manager = TakeOffNode.new
-      @node_manager.start_nodes(10.times.map { |i| 27600 + i } )
+      @node_manager = TakeOffNode.new(27260)
     end
 
     after(:all) do
@@ -161,14 +152,11 @@ RSpec.describe TakeOffClient do
     end
 
     it 'only valid bookings are accepted' do
-      n = 30
-      clients = n.times.map do |i|
-        client(@node_manager.nodes.values.sample)
-      end
+      @node_manager.start_nodes(10)
 
       # Create a flight
       flight = FactoryBot.build(:flight, datetime: "2025-06-24T00:00:00Z", seats: { window: 2, aisle: 0, between_seats: 0 })
-      response = clients.first.flights_create(flight)
+      response = @node_manager.some_client.flights_create(flight)
       expect(response).to include("status" => "ok")
       flight_id = response['value']['id']
 
@@ -176,12 +164,14 @@ RSpec.describe TakeOffClient do
 
       booking = FactoryBot.build(:booking, flight_id: flight_id, user: "user", seats: { window: 1, aisle: 0, between_seats: 0 })
 
-      responses = Parallel.map(clients, in_processes: n) do |client|
+      # Try to create 10 bookings in parallel
+      responses = Parallel.map(@node_manager.clients, in_processes: 16) do |client|
         client.bookings_create(booking)
       end
 
+      # Only 2 bookings should be accepted
       expect( responses.count{ |r| r["status"] == "booking_accepted"} ).to eq(2)
-      expect( responses.count{ |r| ["booking_denied", "flight_closed"].include?(r["status"]) }).to eq(n-2)
+      expect( responses.count{ |r| ["booking_denied", "flight_closed"].include?(r["status"]) }).to eq(8)
     end
   end
 end
